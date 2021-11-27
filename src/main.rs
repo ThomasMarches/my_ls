74a02887d47eb86f32fcc5370558cb11c03acb2e
Thomas Marches <56929277+ThomasMarches@users.noreply.github.com>
2021-11-28 00:55:33 +0100

    refactor: implement Default for FolderResult

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,19 @@ pub struct FolderResult {
     empty_lines: i32,
 }
 
+impl Default for FolderResult {
+    fn default() -> Self {
+        Self {
+            file_number: 0,
+            code_file_number: 0,
+            comment_lines: 0,
+            lines: 0,
+            code_lines: 0,
+            empty_lines: 0,
+        }
+    }
+}
+
 impl Debug for FolderResult {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
         write!(
@@ -21,19 +34,6 @@ impl Debug for FolderResult {
             "1 - Files: {},\n2 - Code files: {},\n3 - Lines: {},\n4 - Code lines: {},\n5 - Comment lines: {},\n6 - Blank lines: {}",
             self.file_number, self.code_file_number, self.lines, self.code_lines, self.comment_lines, self.empty_lines,
         )
-    }
-}
-
-fn process_folders(paths: &Vec<String>, result: &mut FolderResult) {
-    for path in paths {
-        let buffer = match fs::read_to_string(path) {
-            Ok(buffer) => buffer,
-            Err(_) => continue,
-        };
-        let lines = buffer.lines();
-        result.lines += lines.to_owned().count() as i32;
-        result.file_number += 1;
-        tool::process_file(path, &lines, result);
     }
 }
 
@@ -48,6 +48,19 @@ fn show_usage() {
         }
     };
     println!("{}", help_text);
+}
+
+fn process_folders(paths: &Vec<String>, result: &mut FolderResult) {
+    for path in paths {
+        let buffer = match fs::read_to_string(path) {
+            Ok(buffer) => buffer,
+            Err(_) => continue,
+        };
+        let lines = buffer.lines();
+        result.lines += lines.to_owned().count() as i32;
+        result.file_number += 1;
+        tool::process_file(path, &lines, result);
+    }
 }
 
 fn main() {
@@ -65,15 +78,7 @@ fn main() {
         }
     };
 
-    let mut result = FolderResult {
-        file_number: 0,
-        code_file_number: 0,
-        comment_lines: 0,
-        code_lines: 0,
-        empty_lines: 0,
-        lines: 0,
-    };
-
+    let mut result = FolderResult::default();
     let paths = tool::get_files_names(&arg);
     process_folders(&paths, &mut result);
     println!("{:?}", result);
